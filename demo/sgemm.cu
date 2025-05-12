b6d387ec3f42f41fa5f18802b0269d3c49b19ce1
root <root@notebook-devenviron-0508-101625-gpu-15rdz77-launcher-0.notebook-devenviron-0508-101625-gpu-15rdz77.ns-97586f53a56c4307adda2a538fae792b.svc.cluster.local>
2025-05-12 09:58:59 +0000

    5/12

--- demo/sgemm.cu
+++ demo/sgemm.cu
@@ -1,6 +1,22 @@
 #include <cuda_runtime.h>
 #include <bits/stdc++.h>
 
+#define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
+
+template <int ROWS, int COLS>
+__device__ void matrix_print(const float mat[ROWS][COLS], const char* name) {
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
+        printf("=== %s ===\n", name);
+        for (int i = 0; i < ROWS; ++i) {
+            for (int j = 0; j < COLS; ++j) {
+                printf("%8.2f ", mat[i][j]);
+            }
+            printf("\n");
+        }
+        printf("================\n");
+    }
+}
+
 __global__ void sgemm_naive_v0(float* a, float* b, float* c, int M, int N, int K) {
     int bx = blockIdx.x, by = blockIdx.y;
     int tx = threadIdx.x, ty = threadIdx.y;
@@ -17,43 +33,24 @@ __global__ void sgemm_naive_v0(float* a, float* b, float* c, int M, int N, int K
     }
 }
 
-template <int ROWS, int COLS>
-__device__ void matrix_print(const float mat[ROWS][COLS], const char* name) {
-    if (threadIdx.x == 0 && threadIdx.y == 0) {
-        printf("=== %s ===\n", name);
-        for (int i = 0; i < ROWS; ++i) {
-            for (int j = 0; j < COLS; ++j) {
-                printf("%8.2f ", mat[i][j]);
-            }
-            printf("\n");
-        }
-        printf("================\n");
-    }
-}
-
-
 template <
-    const int SRAM_M,
-    const int SRAM_N,
-    const int SRAM_K
+    const int BLOCK_TILE_M,
+    const int BLOCK_TILE_N,
+    const int BLOCK_TILE_K
 >
 __global__ void sgemm_block_tile_v1(float *a, float* b, float* c, int M, int N, int K) {
     int bx = blockIdx.x, by = blockIdx.y;
     int tx = threadIdx.x, ty = threadIdx.y;
 
-    __shared__ float smem_a[SRAM_M][SRAM_K];
-    __shared__ float smem_b[SRAM_K][SRAM_N];
+    __shared__ float smem_a[BLOCK_TILE_M][BLOCK_TILE_K];
+    __shared__ float smem_b[BLOCK_TILE_K][BLOCK_TILE_N];
 
-    // if (blockIdx.x == 1 && blockIdx.y == 1 && threadIdx.x == 0 && threadIdx.y == 0) {
-    //     matrix_print<SRAM_M, SRAM_K>(smem_a, "smem_a");
-    //     matrix_print<SRAM_K, SRAM_N>(smem_b, "smem_b");
-    // }
-    for (int k_outter = 0; k_outter < K; k_outter += SRAM_K) {
-        int global_row_a = by * SRAM_M;
-        int global_col_a = bx * SRAM_K;
+    for (int k_outter = 0; k_outter < K; k_outter += BLOCK_TILE_K) {
+        int global_row_a = by * BLOCK_TILE_M;
+        int global_col_a = k_outter;
 
-        for (int m = ty; m < SRAM_M; m += blockDim.y) {
-            for (int k = tx; k < SRAM_K; k += blockDim.x) {
+        for (int m = ty; m < BLOCK_TILE_M; m += blockDim.y) {
+            for (int k = tx; k < BLOCK_TILE_K; k += blockDim.x) {
                 int row = global_row_a + m;
                 int col = global_col_a + k;
                 if (row < M && col < K) {
@@ -62,11 +59,11 @@ __global__ void sgemm_block_tile_v1(float *a, float* b, float* c, int M, int N, 
             }
         }
 
-        int global_row_b = by * SRAM_K;
-        int global_col_b = bx * SRAM_N;
+        int global_row_b = k_outter;
+        int global_col_b = bx * BLOCK_TILE_N;
 
-        for (int k = ty; k < SRAM_K; k += blockDim.y) {
-            for (int n = tx; n < SRAM_N; n += blockDim.x) {
+        for (int k = ty; k < BLOCK_TILE_K; k += blockDim.y) {
+            for (int n = tx; n < BLOCK_TILE_N; n += blockDim.x) {
                 int row = global_row_b + k;
                 int col = global_col_b + n;
                 if (row < K && col < N) {
@@ -75,11 +72,117 @@ __global__ void sgemm_block_tile_v1(float *a, float* b, float* c, int M, int N, 
             }
         }
         __syncthreads();
-        int global_row_c = by * SRAM_M;
-        int global_col_c = bx * SRAM_N;
+
+        int global_row_c = by * BLOCK_TILE_M;
+        int global_col_c = bx * BLOCK_TILE_N;
         
+        for (int m = ty; m < BLOCK_TILE_M; m += blockDim.y) {
+            for (int n = tx; n < BLOCK_TILE_N; n += blockDim.x) {
+                float acc = 0.0f;
+                for (int k = 0; k < BLOCK_TILE_K; k++) {
+                    acc += smem_a[m][k] * smem_b[k][n];
+                }
+                int row = global_row_c + m;
+                int col = global_col_c + n;
+                if (row < M && col < N) {
+                    float old = (k_outter == 0) ? 0.0f : c[row * N + col];
+                    c[row * N + col] = old + acc;
+                }
+            }
+        }
+    }
+}
+
+// 代码设计哲学，从"计算需求推导线程配置"而不是"先设定线程配置，再适配数据"
+// 每个block要处理的数据为：(BLOCK_TILE_M, BLOCK_TILE_K) @ (BLOCK_TILE_K, BLOCK_TILE_N) -> (BLOCK_TILE_M, BLOCK_TILE_N)
+// 每个thread要处理的数据为：(THREAD_TILE_M, 1) @ (1, THREAD_TILE_N) -> (THREAD_TILE_M, THREAD_TILE_N)
+// 根据以上两点得到block中线程的维度为：(BLOCK_TILE_M/THREAD_TILE_M, BLOCK_TILE_N/THREAD_TILE_N)
+// 注意：这里BLOCK_TILE_M一定是THREAD_TILE_M整数倍，BLOCK_TILE_N同理
+template<
+    const int BLOCK_TILE_M,
+    const int BLOCK_TILE_N,
+    const int BLOCK_TILE_K,
+    const int THREAD_TILE_M,
+    const int THREAD_TILE_N
+>
+__global__ void sgemm_thread_tile_v2(float* a, float* b, float* c, int M, int N, int K) {
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
+
+    __shared__ float smem_a[BLOCK_TILE_M][BLOCK_TILE_K];
+    __shared__ float smem_b[BLOCK_TILE_K][BLOCK_TILE_N];
+
+    float reg_c[THREAD_TILE_M][THREAD_TILE_N] = {0};
+    float reg_a[THREAD_TILE_M];
+    float reg_b[THREAD_TILE_N];
+
+    const int THREAD_Y_PER_BLOCK = BLOCK_TILE_M / THREAD_TILE_M;
+    const int THREAD_X_PER_BLOCK = BLOCK_TILE_N / THREAD_TILE_N;
+
+    for (int k_outter = 0; k_outter < K; k_outter += BLOCK_TILE_K) {
+        int global_row_a = by * BLOCK_TILE_M;
+        int global_col_a = k_outter;
+
+        for (int m = ty; m < BLOCK_TILE_M; m += THREAD_Y_PER_BLOCK) {
+            for (int k = tx; k < BLOCK_TILE_K; k += THREAD_X_PER_BLOCK) {
+                int row = global_row_a + m;
+                int col = global_col_a + k;
+                if (row < M && col < K) {
+                    smem_a[m][k] = a[row * K + col];
+                }
+            }
+        }
+
+        int global_row_b = k_outter;
+        int global_col_b = bx * BLOCK_TILE_N;
+
+        for (int k = ty; k < BLOCK_TILE_K; k += THREAD_Y_PER_BLOCK) {
+            for (int n = tx; n < BLOCK_TILE_N; n += THREAD_X_PER_BLOCK) {
+                int row = global_row_b + k;
+                int col = global_col_b + n;
+                if (row < K && col < N) {
+                    smem_b[k][n] = b[row * N + col];
+                }
+            }
+        }
+        __syncthreads();
+
+        for (int k_inner = 0; k_inner < BLOCK_TILE_K; k_inner++) {
+            int shared_row_a = ty * THREAD_TILE_M;
+            for (int m = 0; m < THREAD_TILE_M; m++) {
+                int row = shared_row_a + m;
+                int col = k_inner;
+                reg_a[m] = smem_a[row][col];
+            }
+
+            int shared_col_b = tx * THREAD_TILE_N;
+            for (int n = 0; n < THREAD_TILE_N; n++) {
+                int row = k_inner;
+                int col = shared_col_b + n;
+                reg_b[n] = smem_b[row][col];
+            }
+            
+            for (int m = 0; m < THREAD_TILE_M; m++) {
+                for (int n = 0; n < THREAD_TILE_N; n++) {
+                    reg_c[m][n] += reg_a[m] * reg_b[n];
+                }
+            }
+        }
+        __syncthreads();
     }
 
+    int global_row_c = by * BLOCK_TILE_M + ty * THREAD_TILE_M;
+    int global_col_c = bx * BLOCK_TILE_N + tx * THREAD_TILE_N;
+
+    for (int m = 0; m < THREAD_TILE_M; m++) {
+        for (int n = 0; n < THREAD_TILE_N; n++) {
+            int row = global_row_c + m;
+            int col = global_col_c + n;
+            if (row < M && col < N) {
+                c[row * N + col] = reg_c[m][n];
+            }
+        }
+    }
 }
 
 void cpu_sgemm(float *a, float* b, float* c, int M, int N, int K) {
@@ -114,7 +217,7 @@ void test_result(float*a, float* b, float* c, int M, int N, int K) {
 }
 
 int main() {
-    int M = 16, N = 16, K = 8;
+    int M = 16, N = 16, K = 16;
 
     // host malloc
     float* a = (float* )malloc(sizeof(float) * M * K);
@@ -123,19 +226,19 @@ int main() {
 
     srand((unsigned)time(NULL));
 
-    int num = 0.0f;
+    // int num = 0.0f;
     for (int m = 0; m < M; m++) {
         for (int k = 0; k < K; k++) {
-            // a[m * K + k] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-            a[m * K + k] = num++;
+            a[m * K + k] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+            // a[m * K + k] = num++;
         }
     }
 
-    num = 0.0f;
+    // num = 0.0f;
     for (int k = 0; k < K; k++) {
         for (int n = 0; n < N; n++) {
-            // b[k * N + n] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-            b[k * N + n] = num++;
+            b[k * N + n] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+            // b[k * N + n] = num++;
         }
     }
 
@@ -162,21 +265,38 @@ int main() {
     //     test_result(a, b, c, M, N, K);
     // }
 
+    // {
+    //     // copy from host to device
+    //     cudaMemcpy(a_d, a, sizeof(float) * M * K, cudaMemcpyHostToDevice);
+    //     cudaMemcpy(b_d, b, sizeof(float) * K * N, cudaMemcpyHostToDevice);
+
+    //     // compute
+    //     dim3 block(16, 16);
+    //     dim3 grid((N + 16 - 1) / 16, (M + 16 - 1) / 16);
+    //     sgemm_block_tile_v1<16, 16, 16><<<grid, block>>>(a_d, b_d, c_d, M, N, K);
+
+    //     // write back
+    //     cudaMemcpy(c, c_d, sizeof(float) * M * N, cudaMemcpyDeviceToHost);
+
+    //     // check
+    //     test_result(a, b, c, M, N, K);
+    // }
+
     {
         // copy from host to device
         cudaMemcpy(a_d, a, sizeof(float) * M * K, cudaMemcpyHostToDevice);
         cudaMemcpy(b_d, b, sizeof(float) * K * N, cudaMemcpyHostToDevice);
 
         // compute
-        dim3 block(2, 2);
-        dim3 grid((N + 2 - 1) / 2, (M + 8 - 1) / 2);
-        sgemm_block_tile_v1<4, 4, 4><<<grid, block>>>(a_d, b_d, c_d, M, N, K);
+        dim3 block(16, 16);
+        dim3 grid((N + 16 - 1) / 16, (M + 16 - 1) / 16);
+        sgemm_thread_tile_v2<16, 16, 16, 4, 4><<<grid, block>>>(a_d, b_d, c_d, M, N, K);
 
         // write back
         cudaMemcpy(c, c_d, sizeof(float) * M * N, cudaMemcpyDeviceToHost);
 
         // check
-        // test_result(a, b, c, M, N, K);
+        test_result(a, b, c, M, N, K);
     }
 
     // free
